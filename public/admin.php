d00c4b429a7b08af082cd5a3ae3e62f2a530cdaa
arturo resendiz <arturoa.resendiz@gmail.com>
2015-08-25 14:04:19 -0500

    proyecto consolida

--- public/admin.php
+++ public/admin.php
@@ -1,11 +1,23 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php confirm_logged_in(); ?>
 
-<html lang="en">
-	<head>
-		<title>untitled</title>
-	</head>
-	<body>
+<?php $layout_context = "admin"; ?>
+<?php include("../includes/layouts/header.php"); ?>
 
-	</body>
-</html>
+<div id="main">
+  <div id="navigation">
+    &nbsp;
+  </div>
+  <div id="page">
+    <h2>Admin Menu</h2>
+    <p>Welcome to the admin area, <?php echo htmlentities($_SESSION["username"]); ?>.</p>
+    <ul>
+      <li><a href="manage_content.php">Manage Website Content</a></li>
+      <li><a href="manage_admins.php">Manage Admin Users</a></li>
+      <li><a href="logout.php">Logout</a></li>
+    </ul>
+  </div>
+</div>
+
+<?php include("../includes/layouts/footer.php"); ?>
